053f67b50cba330cbe49b6630a1802a353a7bce5
Maui <mauiandsonsph@yahoo.com>
2019-07-23 19:34:26 +0800

    almost done styling user profile for mobile

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -2,38 +2,19 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { fetchUser, removeAMessage } from "../store/actions/user";
-import { removeMessage } from "../store/actions/messages";
+
 import DefaultProfileImg from "../images/default-profile-image.jpg";
 
 import MessageItem from "./MessageItem";
 import "../css/UserProfile.css";
 
 class UserProfile extends Component {
-  constructor(props) {
-    super(props);
-    //try just setting the state to the user messages instead of the whole user
-    this.state = { messages: [] };
-
-    this.handleRemove = this.handleRemove.bind(this);
-  }
-
   async componentDidMount() {
     const userId = this.props.match.params.id;
-    let user = await this.props.fetchUser(userId);
-    this.setState({ messages: user.messages });
-  }
-
-  handleRemove(currentUser, userId) {
-    this.props.removeMessage(currentUser, userId);
+    await this.props.fetchUser(userId);
   }
 
   render() {
-    if (this.state.user === null) {
-      return <div />;
-    }
-
-    // console.log(this.state.messages);
-    console.log(this.props.messages);
     const {
       profileImageUrl,
       username,
@@ -45,17 +26,9 @@ class UserProfile extends Component {
     if (messages === undefined) {
       return <div />;
     }
-
-    if (profileImageUrl === undefined) {
-      return <div />;
-    }
-    // const {
-    //   profileImageUrl,
-    //   username,
-    //   messages,
-    //   following,
-    //   followers
-    // } = this.state.user;
+    console.log(this.props.foundUser);
+    console.log(followers.length);
+    console.log(following);
 
     const messageList = messages.map(m => (
       <MessageItem
@@ -67,59 +40,78 @@ class UserProfile extends Component {
         text={m.text}
         profileImageUrl={profileImageUrl}
         removeMessage={this.props.removeAMessage.bind(this, m.user, m._id)}
-        // removeMessage={this.props.removeMessage(m.user, m._id)}
         isCorrectUser={this.props.currentUser === m.user}
       />
     ));
 
-    // const messageList = this.state.messages.map(m => (
-    //   <MessageItem
-    //     test={m}
-    //     key={m._id}
-    //     text={m.text}
-    //     date={m.createdAt}
-    //     username={username}
-    //     userId={this.props.foundUser._id}
-    //     currentUserId={this.props.currentUser}
-    //     profileImageUrl={profileImageUrl}
-    //     isCorrectUser={this.props.currentUser === m.user}
-    //     removeMessage={this.props.removeAMessage.bind(this, m.user, m._id)}
-    //   />
-    // ));
     return (
-      <section className="userProfile">
-        <div className="banner" />
-        <div className="userProfileImage">
-          <img
-            src={profileImageUrl || DefaultProfileImg}
-            className="img-fluid rounded-circle"
-            alt={username}
-          />
+      <section>
+        <div className="userProfile">
+          <div className="banner" />
+          <div className="userHeader" />
+          <div className="profilePic">
+            <img
+              src={profileImageUrl || DefaultProfileImg}
+              className="img-fluid rounded-circle"
+              alt={this.props.username}
+            />
+          </div>
+          <div className="userName">
+            <span className="userNameProfile">{username}</span>
+            <span className="userHandleProfile">
+              <Link to={`/users/${this.props.userId}`}>@{username} &nbsp;</Link>
+            </span>
+            <button className="btn btn-success">follow</button>
+          </div>
+          <div className="stats">
+            <ul className="list-group list-group-horizontal">
+              <li className="list-group-item">
+                <p className="stat-title">Tiririts</p>
+                <span className="stat-counts">{messages.length}</span>
+              </li>
+              <li className="list-group-item">
+                <p className="stat-title">Followers</p>
+                <span className="stat-counts">{followers.length}</span>
+              </li>
+              <li className="list-group-item pr-0">
+                <p className="stat-title">Following</p>
+                <span className="stat-counts ">{following.length}</span>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="seperator" />
-        <div className="userScreenName">
-          <span className="userNameProfile">{username}</span>
-          <span className="userHandleProfile">
-            <Link to={`/users/${this.props.userId}`}>@{username} &nbsp;</Link>
-          </span>
-          <button className="btn btn-success">follow</button>
-        </div>
-        <div className="Tiririts mt-1">
-          <span className="stat-title">Tiririts</span>
-          <span className="stat-counts">{messages.length}</span>
-        </div>
-        <div className="Following mt-1">
-          <span className="stat-title">Followers</span>
-          <span className="stat-counts">{followers.length}</span>
-        </div>
-        <div className="Follower mt-1">
-          <span className="stat-title">Following</span>
-          <span className="stat-counts">{following.length}</span>
-        </div>
-        <div className="userProfile-Messages">
-          <ul className="list-group" id="messages">
-            {messageList.reverse()}
-          </ul>
+
+        <div className="smallerUserProfile">
+          <div className="smallerBanner" />
+          <div className="smallerContent" />
+          <div className="smallerProfilePic">
+            <img
+              src={profileImageUrl || DefaultProfileImg}
+              className="img-fluid rounded-circle"
+              alt={username}
+            />
+          </div>
+          <div className="smallerScreenName">
+            <div className="userName">{username}</div>
+            <div className="userHandle">
+              <Link to={`/users/${this.props.userId}`}>@{username} &nbsp;</Link>
+            </div>
+          </div>
+          <div className="followBtn">
+            <button className="btn btn-success">follow</button>
+          </div>
+          <div className="smallerTiririts">
+            <span className="stat-title">Tiririts</span>
+            <span className="stat-counts">{messages.length}</span>
+          </div>
+          <div className="smallerFollowing">
+            <span className="stat-title">Following</span>
+            <span className="stat-counts">{following.length}</span>
+          </div>
+          <div className="smallerFollower">
+            <span className="stat-title">Followers</span>
+            <span className="stat-counts">{followers.length}</span>
+          </div>
         </div>
       </section>
     );
@@ -127,9 +119,7 @@ class UserProfile extends Component {
 }
 
 function mapStateToProps(state) {
-  console.log(state);
   return {
-    messages: state.user.messages,
     currentUser: state.currentUser.user.id,
     foundUser: state.user.user
   };
@@ -137,5 +127,5 @@ function mapStateToProps(state) {
 
 export default connect(
   mapStateToProps,
-  { fetchUser, removeMessage, removeAMessage }
+  { fetchUser, removeAMessage }
 )(UserProfile);
